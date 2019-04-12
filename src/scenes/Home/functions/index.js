9d324fe508b94236cf9d299d0ccd5044d6444a52
SalvadorM <sronikle@gmail.com>
2019-04-12 00:58:33 -0400

    get classes and render

---
+++ src/scenes/Home/functions/index.js
@@ -0,0 +1,8 @@
+import axios from '../../../utilities/axios'
+
+
+export const getCurrentClasses = async (season, year) => {
+
+    let api_url = `/usercourse/userclasses/?season=${season}&year=${year}`
+    return axios.get(api_url)
+}
--- src/scenes/Home/scenes/HomeLanding.js
+++ src/scenes/Home/scenes/HomeLanding.js
@@ -1,5 +1,10 @@
 import React, {Component} from 'react'
 
+
+//functions 
+import {getCurrentClasses} from '../functions/index'
+import UserCourses from '../../courses/scenes/UserCourses';
+
 class Home extends Component {
     constructor(props){
         super(props)
@@ -8,20 +13,63 @@ class Home extends Component {
             username: localStorage.getItem('username'),
             classes: [],
             cbResponce: false,
+            season: 'fall',
+            year: new Date().getFullYear()
 
         }
     }
 
-    componentDidMount(){
-      
+    async componentDidMount(){
+        const {season, year} = this.state
+        this.getUserCourse(season, year)
     }
+    getUserCourse = async (season, year) => {
+        try{
+            const classes = await getCurrentClasses(season, year)
+            //add the rest
+            console.log(classes.data)
+            this.setState({classes: classes.data})
+        }
+        catch(err){
+            console.log(err)
+            //handle error 
+        }
+    }
+
+    onchange = (e) => {
+        e.preventDefault()
+        const {season , year} = this.state
+        const field = e.target.name
+        this.setState({[field]: e.target.value})
+        if(field == 'season'){
+            this.getUserCourse(e.target.value, year)
+        }else{
+            this.getUserCourse(season, e.target.value)
+        }
+    }   
+
     render(){
-        const { username } = this.state
+        const { username, season, year, classes} = this.state
         return(
             <div className="container">
                 <div className="jumbotron">
+                    <div className="col-12-xs">
+                    <select className="form-control" name="season" value={season} onChange={this.onchange}>
+                        <option value="spring">Spring</option>
+                        <option value="fall">Fall</option>
+                        <option value="summer">Summer</option>
+                        <option value="winter">Winter</option>
+                    </select>
+                    <select className="form-control" name="year" value={year} onChange={this.onchange}>
+                        <option value="2019">2019</option>
+                        <option value="2018">2018</option>
+                        <option value="2017">2017</option>
+                        <option value="2016">2016</option>
+                    </select>
+                    </div>
                     <h1>Hello {username}</h1>
                     <h2>Render Current Classes</h2>
+                    <UserCourses courses={classes} />
                     <h3>find friends </h3>
                     <h2>Render Post created by user</h2>
                     <h2>Render Friends</h2>
