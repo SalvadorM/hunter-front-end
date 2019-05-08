8ffa2e6ebcbe8bd738064bdd1985bdd75e43cda7
SalvadorM <sronikle@gmail.com>
2019-05-08 10:13:27 -0400

    upload

--- src/utilities/axios.js
+++ src/utilities/axios.js
@@ -1,7 +1,6 @@
 import axios from 'axios'
 
 const websiteURL = `https://hunter-app-api.herokuapp.com/`
-// const devURL = `http://localhost:8000`
 
 export default axios.create({
     baseURL: websiteURL,
