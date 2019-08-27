90dbf6c98ed05aeb806f1620e08196e2b75fee17
SalvadorM <sronikle@gmail.com>
2019-08-26 23:13:39 -0400

    created functions file for chat

---
+++ src/scenes/messages/functions/index.js
@@ -0,0 +1,18 @@
+import axios from '../../../utilities/axios'
+
+
+export const getChatMessages = async (chatId) => {
+    try {
+
+    } catch(e) {
+
+    }
+}
+
+export const createNewChat = async (otherUser) => {
+    try{
+
+    } catch(e) {
+        
+    }
+}
--- src/scenes/messages/scenes/ChatSession.js
+++ src/scenes/messages/scenes/ChatSession.js
@@ -29,7 +29,9 @@ export default class ChatSessionContainer extends Component {
 	}
 
 
-
+	sendNewMessage = () => {
+		
+	}
 
 
 	render() {
