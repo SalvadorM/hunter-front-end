e8fb896ec458c160c2638ea643cfadea7a65ead7
SalvadorM <sronikle@gmail.com>
2019-09-08 21:25:04 -0400

    credential default for axios calls ;

--- src/utilities/axios.js
+++ src/utilities/axios.js
@@ -3,12 +3,10 @@ import axios from 'axios'
 const websiteURL = `https://hunter-app-api.herokuapp.com/`
 
 // const devURL = `http://localhost:8000`
+axios.defaults.withCredentials = true;
 
 
 export default axios.create({
     baseURL: websiteURL,
     withCredentials: true,
-    headers: {
-      'Content-Type': 'application/json',
-    },
   })
