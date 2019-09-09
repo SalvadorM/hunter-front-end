e6d1bf1323502a18c2e7757ea5f3704c129a37f1
SalvadorM <sronikle@gmail.com>
2019-09-08 22:05:39 -0400

    removed default settings in axios

--- src/utilities/axios.js
+++ src/utilities/axios.js
@@ -3,13 +3,9 @@ import axios from 'axios'
 const websiteURL = `https://hunter-app-api.herokuapp.com/`
 
 // const devURL = `http://localhost:8000`
-axios.defaults.withCredentials = true;
-
+// manually set the cookie header for mobile
 
 export default axios.create({
     baseURL: websiteURL,
     withCredentials: true,
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded'
-    },
   })
