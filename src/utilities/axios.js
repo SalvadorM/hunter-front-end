e8c38a664d3518c6db53cdb8befe7c791387422a
SalvadorM <sronikle@gmail.com>
2019-09-15 23:04:33 -0400

    changed to website url

--- src/utilities/axios.js
+++ src/utilities/axios.js
@@ -1,15 +1,15 @@
 import axios from 'axios'
 
-// const websiteURL = `https://hunter-app-api.herokuapp.com/`
+const websiteURL = `https://hunter-app-api.herokuapp.com/`
 const devURL = `http://localhost:8000`
 // manually set the cookie header for mobile
 const Axios = axios.create({
-    baseURL: devURL,
+    baseURL: websiteURL,
     withCredentials: true,
   })
 
 
-  
+
 Axios.defaults.withCredentials = true
  
 export default Axios
